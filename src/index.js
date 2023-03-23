8b3adc18541ede00b12f9ea24f4589e706d1278c
tabascum <marques_vasco@yahoo.es>
2023-03-23 19:44:06 +0000

    Add container changes for responsiveness

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ const aside = document.querySelector(".aside");
 menu.onclick = () => {
   aside.classList.toggle("aside");
 };
+
+//Set a dynamically changed year
+
+const currentYear = document.querySelector("#currentYear");
+
+currentYear.innerHTML = new Date().getFullYear();
