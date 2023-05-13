430941e50eeef48e9ca2eac45eb63a8dd1e65698
tabascum <marques_vasco@yahoo.es>
2023-05-13 18:40:02 +0200

    Fix the onclick events

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const menu = document.getElementById("menu");
-const aside = document.querySelector(".aside");
+const aside = document.querySelector("aside");
+
 const content = document.querySelector(".aside-content");
 
-menu.onclick = () => (aside.style.display = "block");
+menu.onclick = (e) => {
+  e.preventDefault();
+  aside.style.display = "block";
+};
 
 window.onclick = (e) => {
-  if (e.target == aside) {
+  if (e.target === aside) {
     aside.style.display = "none";
   }
 };
